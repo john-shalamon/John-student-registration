c01d777a5ff6d22b95d2ff37a95675fe84fd4d79
john-shalamon <johnshalamon.cs22@stellamaryscoe.edu.in>
2025-03-21 17:16:50 +0530

    Update metadata description in layout.tsx for clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'John-StudentRegister Form',
-  description: '',
+  description: 'John-StudentRegister Form',
   generator: '',
 }
 
